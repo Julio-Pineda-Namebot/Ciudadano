45d8df1742c981e7b3877f946b686125a26f77ef
Julio-Pineda-Namebot <julionicolas548@gmail.com>
2025-07-15 23:13:19 -0500

    fix bug

--- lib/features/home/emergency/presentation/widgets/ver_mapa_page.dart
+++ lib/features/home/emergency/presentation/widgets/ver_mapa_page.dart
@@ -46,7 +46,7 @@ class _VerMapaPageState extends State<VerMapaPage> {
         children: [
           TileLayer(
             urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            userAgentPackageName: "com.example.app",
+            userAgentPackageName: "com.ciudadano.app",
           ),
           MarkerLayer(
             markers: [
