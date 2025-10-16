ab1a8233e58f086ab5bed77e5bcd2007f4091d1a
Julio-Pineda-Namebot <julionicolas548@gmail.com>
2025-10-15 23:15:50 -0500

    Update form incident

--- lib/config/api/api_config.dart
+++ lib/config/api/api_config.dart
@@ -1,6 +1,6 @@
 mixin ApiConfig {
   static const String productionUrl =
-      "https://ciudadano-production.up.railway.app";
+      "https://ciudadano.onrender.com";
   static const String developmentUrl = "http://192.168.0.2:3000";
 
   static String get ac {
