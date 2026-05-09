acc96421d80156da99a0fd90dd920353a2efc60d
Julio-Pineda-Namebot <julionicolas548@gmail.com>
2026-05-09 08:59:28 -0500

    Cambios url

--- lib/config/api/api_config.dart
+++ lib/config/api/api_config.dart
@@ -1,5 +1,5 @@
 mixin ApiConfig {
-  static const String productionUrl = "https://ciudadano-api-rest.onrender.com";
+  static const String productionUrl = "https://ciudadano-rest-api.onrender.com";
   static const String developmentUrl = "http://192.168.0.3:3000";
 
   static String get baseUrl {
